96cb7f912b62d44f60fdff5f9ecf88007475cd05
Hekotoru <rafelito266@gmail.com>
2015-04-13 19:03:13 -0400

    Creacion del proyecto.

---
+++ FBF_Proyect/InFile_Module/INFILE.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FBF_Project
+{
+    /// <summary>
+    /// Clase encargada de leer y almacenar los caracteres del archivo de entrada
+    /// </summary>
+    class INFILE
+    {
+  
+        /// <summary>
+        /// Carga el archivo de entrada y lo almacena en una lista de strings
+        /// </summary>
+        /// <param name="Name"> Nombre del archivo</param>
+        /// <returns>Una lista de strings de las proposiciones</returns>
+        public List<string> LoadFile(string Name)
+        {
+            List<string> listPreposition = new List<string>();
+           
+            ///Obtiene la direccion del escritorio del usurio que este ejecutando el programa
+            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            filePath += "\\" + Name + ".txt";
+
+            ///Toma cada line del archivo y lo guarda en un arreglo.
+            try
+            {
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    String line = sr.ReadToEnd();
+                    listPreposition.Add(line);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("El archivo no puede ser leido o nombre incorrecto");
+                Console.WriteLine(e.Message);
+            }
+
+            return listPreposition;
+        }
+
+    }
+}
---
+++ FBF_Proyect/Tokenizer/Token.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FBF_Proyect.Tokenizer
+{
+    public class Token
+    {
+      
+        public static int TERMINAL { get { return -1; } }
+        public static int OPERATOR { get { return 1; } }
+        public static int OPEN_BRACKET { get { return 2; }}
+        public static int CLOSE_BRACKET { get { return 3; } }
+        public static int STRING { get { return 4; } }
+        
+
+
+        public int TokenType { get; private set; }
+        public String Sequence{ get; private set; }
+
+        public Token(int token, String sequence){
+
+            this.TokenType = token;
+            this.Sequence = sequence;
+        }
+    }
+}
