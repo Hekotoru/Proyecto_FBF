e2d5c865d77a4a548fb9fc9dfcb85c45c9eab914
Hekotoru <rafelito266@gmail.com>
2015-04-14 17:15:18 -0400

    Version mejorada del proyecto

---
+++ FBF_Proyect/Analyzer_Syntax/Analyzer_Syntax.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace FBF_Proyect.Analyzer_Syntax
+{
+    public class Analyzer_Syntax
+    {
+        /*
+         * C++ Program to check for balanced parentheses in an expression using stack.
+         * Given an expression as string comprising of opening and closing characters
+         * of parentheses - (), curly braces - {} and square brackets - [], we need to 
+         * check whether symbols are balanced or not. 
+         */
+
+        // Function to check whether two characters are opening 
+        // and closing of same type. 
+        bool ArePair(char opening,char closing)
+        {
+            if(opening == '(' && closing == ')') return true;
+            else if(opening == '{' && closing == '}') return true;
+            else if(opening == '[' && closing == ']') return true;
+            return false;
+        }
+        public bool AreParanthesesBalanced(string exp)
+        {
+            Stack<char>  S = new Stack<char>();
+            for(int i =0;i<exp.Length;i++)
+            {
+                if(exp[i] == '(' || exp[i] == '{' || exp[i] == '[')
+                    S.Push(exp[i]);
+                else if(exp[i] == ')' || exp[i] == '}' || exp[i] == ']')
+                {
+                    if(S.Count ==0 || !ArePair(S.First(),exp[i]))
+                        return false;
+                    else
+                        S.Pop();
+                }
+            }
+            return S.Count == 0 ? true:false;
+        }
+ 
+
+    }
+}
--- FBF_Proyect/Program.cs
+++ FBF_Proyect/Program.cs
@@ -10,6 +10,22 @@ namespace FBF_Proyect
     {
         static void Main(string[] args)
         {
+           /*Code to test the function AreParanthesesBalanced*/
+            string expression;
+            Analyzer_Syntax.Analyzer_Syntax Prueba = new Analyzer_Syntax.Analyzer_Syntax();
+            Console.WriteLine("Enter an expression:  "); // input expression from STDIN/Console
+            expression=Console.ReadLine();
+            if (Prueba.AreParanthesesBalanced(expression))
+            {
+                Console.WriteLine("Balanced\n");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Not Balanced\n");
+                Console.ReadKey();
+            }
+
         }
     }
 }
